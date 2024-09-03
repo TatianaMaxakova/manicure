40fccad36a6d429d58b6851e9548a18654004709
Tatyana M <t11098750@gmail.com>
2024-09-03 21:03:05 +0300

    Database Set up
    postgres=# create user manicure with password '****';
    CREATE ROLE
    postgres=# create database manicure with owner manicure;
    CREATE DATABASE
    postgres=# alter user manicure createdb;
    ALTER ROLE
    postgres=#
    
    added to ~/.pgpass
    added to ~/.pg_service.conf
    
    python manage.py migrate
    python manage.py createsuperuser

--- project/manicure/settings.py
+++ project/manicure/settings.py
@@ -74,10 +74,12 @@ WSGI_APPLICATION = 'manicure.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+            'service': 'manicure_service',
+        },
     }
-}
+}  
 
 
 # Password validation
